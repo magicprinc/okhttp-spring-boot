6ba07a5b30688151c4d62328d6cb588a090e0f5f
A.Fink <aprpda@gmail.com>
2025-11-30 03:22:09 +0300

    ScopedValueTimeoutInterceptorTest

--- okhttp-spring-boot-autoconfigure/src/test/java/io/freefair/spring/okhttp/autoconfigure/ScopedValueTimeoutInterceptorTest.java
+++ okhttp-spring-boot-autoconfigure/src/test/java/io/freefair/spring/okhttp/autoconfigure/ScopedValueTimeoutInterceptorTest.java
@@ -1,11 +1,16 @@
 package io.freefair.spring.okhttp.autoconfigure;
 
+import io.freefair.spring.okhttp.client.OkHttpClientRequest;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import org.junit.jupiter.api.Test;
 import org.junitpioneer.jupiter.SetSystemProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +19,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(classes = ScopedValueTimeoutInterceptorTest.class)
 @EnableAutoConfiguration
 @SetSystemProperty(key ="okhttp.addDefaultInterceptors", value = "true")
+@SetSystemProperty(key ="okhttp.connectTimeout", value = "41s")
+@SetSystemProperty(key ="okhttp.readTimeout", value = "45s")
 class ScopedValueTimeoutInterceptorTest {
     enum Foo { A, B, C }
 
@@ -21,9 +28,25 @@ class ScopedValueTimeoutInterceptorTest {
     OkHttpClient okHttpClient;
 
     @Test
-    void basic() {
+    void basic() throws Exception {
         assertSame(Foo.A.getClass(), Foo.B.getClass());
         assertNotNull(okHttpClient);
+
+        Request req = OkHttpClientRequest.buildRequest().url("https://www.coachoutlet.com/").get().build();
+
+        var resp = okHttpClient.newCall(req).execute();
+        assertEquals(403, resp.code());
+        System.out.println(resp);
+
+        var e = assertThrows(IOException.class, ()->
+            ScopedValue
+                .where(ScopedValueTimeoutInterceptor.CONNECT_TIMEOUT, Duration.ofMillis(1))
+                .where(ScopedValueTimeoutInterceptor.READ_TIMEOUT, Duration.ofMillis(1))
+                .call(()->
+                    okHttpClient.newCall(req).execute()
+                )
+        );
+        assertEquals("java.net.SocketTimeoutException: timeout", e.toString());// is InterruptedIOException is IOException
     }
 
 }
